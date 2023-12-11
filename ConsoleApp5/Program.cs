e0b9de1e3380f9eef054f369aa32e2870e17c2dc
Banka24 <Aq567489012@gmail.com>
2023-12-11 10:44:26 +0300

    .

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "asd43 34";
+            bool result = false;
+            if (username.Length > 4 && username.Length < 16)
+            {
+                foreach (char i in username)
+                {
+                    if (!Char.IsUpper(i) && Char.IsWhiteSpace(i) != ' ')
+                    {
+                        result = true;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
