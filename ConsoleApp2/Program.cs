3b5920f0fb9dd802429d50f25fe6b321b2a210cd
Banka24 <Aq567489012@gmail.com>
2023-12-11 10:27:04 +0300

    .

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,35 +8,16 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void Delete<T>(ref T[] array, int index)
-        {
-            T[] newArray = new T[array.Length - 1];
-            for(int i = 0; i < index; i++)
-            {
-                newArray[i] = array[i];
-            }
-            for(int i = index; i < newArray.Length; i++)
-            {
-                newArray[i] = array[i + 1];
-            }
-            array = newArray;
-        }
-
-        static void DeleteFirst<T>(ref T[] array)
-        {
-            Delete(ref array, 0);
-        }
-
-        static void DeleteLast<T>(ref T[] array)
-        {
-            Delete(ref array, array.Length - 1);
-        }
-
+        
         static void Main(string[] args)
         {
-            int[] myArray = { 3, 5, 6, 7, 8 };
+            string str = "Hello";
 
-            DeleteLast(ref myArray);
+            foreach(char i in str)
+            {
+                Console.WriteLine(i);
+            }
+           
         }
     }
 }
